30a8b471b08cb92f9cad48e98aa7bf53f92a84d9
Ilya Fedin <fedin-ilja2010@ya.ru>
2020-01-17 09:10:50 +0400

    don't load vm video drivers

--- configuration.nix
+++ configuration.nix
@@ -229,6 +229,7 @@ with lib;
   services.xserver.enable = true;
   services.xserver.dpi = 120;
   services.xserver.layout = "us,ru";
+  services.xserver.videoDrivers = [ "modesetting" ];
   services.xserver.libinput.enable = true;
 
   services.xserver.displayManager.sddm.enable = true;
