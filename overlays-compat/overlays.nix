83a064f99a11a1f4d6aea171f1287a1397202066
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-01 11:33:19 +0400

    Fix non-flake nix commands

--- overlays-compat/overlays.nix
+++ overlays-compat/overlays.nix
@@ -6,14 +6,10 @@ let
     url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
     sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
   });
-  # Get inputs
-  inputs = (flake-compat {src = ./..;}).defaultNix.inputs;
-  # Using the nixos plumbing that's used to evaluate the config...
-  eval = import <nixpkgs/nixos/lib/eval-config.nix>;
-  # Evaluate the config,
-  paths = (eval {modules = [(import <nixos-config>)]; specialArgs = {inherit inputs;};})
-    # then get the `nixpkgs.overlays` option.
-    .config.nixpkgs.overlays
+  # Get outputs
+  outputs = (flake-compat {src = ./..;}).defaultNix.outputs;
+  # then get the `nixpkgs.overlays` option.
+  paths = outputs.nixosConfigurations.nixos.config.nixpkgs.overlays
   ;
 in
 foldl' (flip extends) (_: super) paths self
