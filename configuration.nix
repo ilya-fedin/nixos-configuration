80c35d013c627b40e170f2936294f7cb5d3494f6
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-04-26 23:52:30 +0400

    Override xdg-desktop-portal-gtk attrs to avoid gtk4

--- configuration.nix
+++ configuration.nix
@@ -340,7 +340,10 @@ with lib;
   xdg.portal.enable = true;
   xdg.portal.gtkUsePortal = true;
   xdg.portal.extraPortals = with pkgs; [
-    xdg-desktop-portal-gtk
+    (xdg-desktop-portal-gtk.overrideAttrs(_: {
+      buildInputs = [ glib gsettings-desktop-schemas gtk3 ];
+      configureFlags = [];
+    }))
     xdg-desktop-portal-kde
   ];
 
