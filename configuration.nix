b098b55f5c1c21be0d49013345c1e5d6e368eeff
Ilya Fedin <fedin-ilja2010@ya.ru>
2026-05-24 21:09:44 +0400

    Set TLP to switch to power-saver profile on battery

--- configuration.nix
+++ configuration.nix
@@ -447,6 +447,7 @@ with lib;
     enable = true;
     pd.enable = true;
     settings = {
+      TLP_PROFILE_BAT = "SAV";
       CPU_SCALING_GOVERNOR_ON_BAT = "conservative";
       CPU_BOOST_ON_AC = "0";
       CPU_BOOST_ON_BAT = "0";
