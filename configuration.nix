838a6306bd9004c6137756a85ae75c588de902bd
Ilya Fedin <fedin-ilja2010@ya.ru>
2026-09-15 03:13:19 +0400

    Fix `evaluation warning: 'system' has been renamed to/replaced by 'stdenv.hostPlatform.system'`

--- configuration.nix
+++ configuration.nix
@@ -324,7 +324,7 @@ in {
 
   programs.firefox = optionalAttrs (hostname == "asus-x421da" || hostname == "ms-7c94") {
     enable = true;
-    package = inputs.firefox.packages.${pkgs.system}.firefox-devedition-bin;
+    package = inputs.firefox.packages.${system}.firefox-devedition-bin;
     languagePacks = [ "ru" ];
     nativeMessagingHosts.packages = with pkgs; [
       firefoxpwa
